e35a80ca99a5161f38833e1af540c6f09a42b014
Len Taing <lentaing@mail.dfci.harvard.edu>
2022-07-10 04:13:23 +0000

    WES Monitor- instance.py- get_instance_ip_from_name- returns internal_ip instead of external ip

--- instance.py
+++ instance.py
@@ -157,8 +157,9 @@ def get_instance_ip(compute, instance_id, project, zone):
 
 def get_instance_ip_from_name(compute, machine_name, project, zone):
     result = get_instance_from_name(compute, machine_name, project, zone)
-    ip_addr = result['networkInterfaces'][0]['accessConfigs'][0]['natIP']
-    return ip_addr
+    ext_ip_addr = result['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+    int_ip_addr = result['networkInterfaces'][0]['networkIP']
+    return int_ip_addr
 
 def get_disk_device_name(compute, instance_id, project, zone, disk_name):
     "Tries to find the disk's assigned device name"
